4c185b77cce3bc2dd7d1128ff2ac2ad19c086f04
LD Tamang <tamangld@gandalf-dev.it.deakin.edu.au>
2025-08-14 10:04:40 +1000

    feat: Add sliding window augmentation by padding method

--- mainrun/utils/dataset_utils.py
+++ mainrun/utils/dataset_utils.py
@@ -1,5 +1,11 @@
+import re
 
-def pad_sequence(words, context_size, pad_mode="cyclic_padding", pad_token="<pad>", align="left"):
+kwargs = {
+    "pad_mode": "cyclic_padding",  # default padding mode
+    "pad_token": "<pad>",           # default padding token
+    "align": "left"                 # default alignment for padding
+}
+def pad_sequence(words, context_size, **kwargs):
     """
     Pad the fixed length sequence to context_size with specified padding mode.
     Args:
@@ -17,17 +23,42 @@ def pad_sequence(words, context_size, pad_mode="cyclic_padding", pad_token="<pad
     if pad_amount == 0:
         return words[:context_size]
 
-    if pad_mode == "cyclic_padding":
+    if kwargs["pad_mode"] == "cyclic_padding":
         padded_seq = (words * ((context_size + len_words - 1) // len_words))[:context_size]
         return padded_seq
 
-    elif pad_mode == "edge_padding":
-        filler = [words[0]] * pad_amount if align == "left" else [words[-1]] * pad_amount
-        return (filler + words) if align == "left" else (words + filler)
+    elif kwargs["pad_mode"] == "edge_padding":
+        filler = [words[0]] * pad_amount if kwargs["align"] == "left" else [words[-1]] * pad_amount
+        return (filler + words) if kwargs["align"] == "left" else (words + filler)
 
-    elif pad_mode == "token_padding":
-        filler = [pad_token] * pad_amount
-        return (filler + words) if align == "left" else (words + filler)
+    elif kwargs["pad_mode"] == "token_padding":
+        filler = [kwargs["pad_token"]] * pad_amount
+        return (filler + words) if kwargs["align"] == "left" else (words + filler)
     else:
-        raise ValueError(f"Unknown pad_mode: {pad_mode}. Supported modes: cyclic_padding, edge_padding, and token_padding.")    
+        raise ValueError(f"Unknown pad_mode")    
+
+
+
+def sliding_window(titles, context_size,
+):
+    """
+    Returns list of text contexts of length = context_size.
+    Args:
+        titles (list): list of titles to process
+        context_size (int): size of the sliding window
+    """
+    contexts = []
+    for title in titles:
+        words = re.sub(r"\s+", " ", title.strip()).split()
+        len_words = len(words)
+        if len_words == 0:
+            continue
+
+        if len_words < context_size:
+            ctx_words = pad_sequence(words, context_size, **kwargs)
+            contexts.append(" ".join(ctx_words))
+        else:
+            for i in range(len_words - context_size + 1):
+                contexts.append(" ".join(words[i:i + context_size]))
+    return contexts
 
